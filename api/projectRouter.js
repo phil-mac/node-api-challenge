47aa74ba4220706aa219e5ab10ce9a3e0e1b015c
Phillip MacEachron <phillipmaceachron@Phillips-MacBook-Pro.local>
2020-01-17 09:31:04 -0800

    added endpoints for adding action to project and getting actions for a project

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const projectModel = require('../data/helpers/projectModel')
+const actionModel = require('../data/helpers/actionModel')
 
 const router = express.Router()
 
@@ -14,13 +15,23 @@ router.get('/', (req, res) => {
         })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateProjectId, (req, res) => {
     projectModel.get(req.params.id)
         .then(project => {
             res.status(200).json(project)
         })
         .catch(() => {
             res.status(500).json({error: "failed to get project"})
+        })
+})
+
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    projectModel.getProjectActions(req.params.id)
+        .then(actions => {
+            res.status(200).json(actions)
+        })
+        .catch(() => {
+            res.status(500).json({error: "failed to get actions"})
         })
 })
 
@@ -34,7 +45,17 @@ router.post('/', (req, res) => {
         })
 })
 
-router.put('/:id', (req, res) => {
+router.post('/:id/actions', validateProjectId, (req, res) => {
+    actionModel.insert({...req.body, project_id: req.params.id})
+        .then(newAction => {
+            res.status(200).json(newAction)
+        })
+        .catch(() => {
+            res.status(500).json({error: "failed to add action"})
+        })
+})
+
+router.put('/:id', validateProjectId, (req, res) => {
     projectModel.update(req.params.id, req.body)
         .then(updatedProject => {
             res.status(200).json(updatedProject)
@@ -44,7 +65,7 @@ router.put('/:id', (req, res) => {
         })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateProjectId, (req, res) => {
     projectModel.remove(req.params.id)
         .then(numRecsDeleted => {
             res.status(200).json(numRecsDeleted)
@@ -53,5 +74,19 @@ router.delete('/:id', (req, res) => {
             res.status(500).json({error: "failed to delete project"})
         })
 })
+
+function validateProjectId(req, res, next){
+    projectModel.get(req.params.id)
+        .then(project => {
+            if (project){
+                next()
+            }else{
+                res.status(500).json({error: "failed to find project with that ID"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "failed to get project with that ID"})
+        })
+}
 
 module.exports = router
